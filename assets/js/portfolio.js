bd92bf8c9f66d7cec8ea7ca037267db933b6f3da
Notesong <notesong.git@outlook.com>
2019-11-11 20:53:17 -0500

    Completed description slideout

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,67 +1,69 @@
 // array of my projects
 const projectDescriptions = [
-    {1: 
-        {
-            projectTitle: 'Tipsease',
-            projectDesc: 'Tip your server in three easy and secure steps.',
-            projectTechStack: 'Built using HTML, LESS/CSS, and Javascript.',
-            projectLink: 'https://tipsease-buildweek.github.io/Lee-UI/',
-            projectGithubRepo: 'https://github.com/Tipsease-Buildweek/Lee-UI',
-            projectHighlights: {
-                1: 'Designed and programmed the landing-page.',
-                2: 'Used responsive LESS/CSS and JavaScript to provide the layout and functionality.'  
-            }
-        }
+    {
+        projectTitle: 'Tipsease',
+        projectDesc: 'Tip your server in three easy and secure steps.',
+        projectTechStack: 'Built using HTML, LESS/CSS, and Javascript.',
+        projectLink: 'https://tipsease-buildweek.github.io/Lee-UI/',
+        projectGithubRepo: 'https://github.com/Tipsease-Buildweek/Lee-UI',
+        projectHighlights: ['Designed and coded the landing-page.', 'Used responsive LESS/CSS and JavaScript to provide the layout and functionality.']
     },
-    {2: 
-        {
-            projectTitle: '',
-            projectDesc: '',
-            projectTechStack: 'Built using...',
-            projectLink: 'http://',
-            projectGithubRepo: 'http://',
-            projectHighlights: {
-                1: '',
-                2: ''  
-            }
+    {
+        projectTitle: '',
+        projectDesc: '',
+        projectTechStack: 'Built using...',
+        projectLink: 'http://',
+        projectGithubRepo: 'http://',
+        projectHighlights: {
+            0: '',
+            1: ''
         }
     }
 ];
 
-function postProjectSlideoutContent () {
-
+const portfolioSlideout = document.querySelector('.portfolio-slideout');
+const projectTitle = document.querySelector('#projectTitle');
+const projectDesc = document.querySelector('#projectDesc');
+const projectTechStack = document.querySelector('#projectTechStack');
+const projectLink = document.querySelector('#projectLink');
+const projectGithubRepo = document.querySelector('#projectGithubRepo');
+const projectHighlights = document.querySelector('#projectHighlights');
+function postProjectSlideoutContent (projectDescriptionNum) {
+    projectTitle.textContent = projectDescriptions[projectDescriptionNum].projectTitle;
+    projectDesc.textContent = projectDescriptions[projectDescriptionNum].projectDesc;
+    projectTechStack.textContent = projectDescriptions[projectDescriptionNum].projectTechStack;
+    projectLink.href = projectDescriptions[projectDescriptionNum].projectLink;
+    projectLink.textContent = projectDescriptions[projectDescriptionNum].projectLink;
+    projectGithubRepo.href = projectDescriptions[projectDescriptionNum].projectGithubRepo;
+    projectGithubRepo.textContent = projectDescriptions[projectDescriptionNum].projectGithubRepo;
+    for(let i = 0 ; i < projectDescriptions[projectDescriptionNum].projectHighlights.length ; i++) {
+        const highlight = document.createElement('li');
+        projectHighlights.appendChild(highlight);
+        highlight.textContent = projectDescriptions[projectDescriptionNum].projectHighlights[i];
+    }
 }
 
-const slideoutToggle = document.querySelector('#x');
-const portfolioSlideout = document.querySelector('.portfolio-slideout');
-const portfolioBoxes = document.querySelectorAll('.portfolio-box');
-const projectImages = document.querySelectorAll('.portfolio-box img');
-
-// slide the project description slideout in and out
-
-portfolioBoxes.forEach(box => {
+// open project description box for active project boxes
+const portfolioBoxes = document.querySelectorAll('.active-box');
+portfolioBoxes.forEach((box, i) => {
     box.addEventListener('click', (event) => {
+        postProjectSlideoutContent(i);
         portfolioSlideout.classList.add('panel--open');
         TweenLite.to(".portfolio-slideout", 1, {right:'0', ease:Power2.easeInOut});
         event.stopPropagation();
     });
 });
 
-slideoutToggle.addEventListener('click', (event) => {
-    if(window.innerWidth > 500 && window.innerWidth < 800) {
-        TweenLite.to(".portfolio-slideout", 1, {right:'-500', ease:Power2.easeInOut});
-    } else if(window.innerWidth >= 800) {
-        TweenLite.to(".portfolio-slideout", 1, {right:'-700', ease:Power2.easeInOut});
-    } else {
-        TweenLite.to(".portfolio-slideout", 1, {right:'-400', ease:Power2.easeInOut});
-    }
+// 'x' to close slideout
+const xSlideoutToggle = document.querySelector('#x');
+xSlideoutToggle.addEventListener('click', (event) => {
+    TweenLite.to(".portfolio-slideout", 1, {right:'-1000', ease:Power2.easeInOut});
     portfolioSlideout.classList.remove('panel--open');
     event.stopPropagation();
 })
 
 // animate mouseover on portfolio images
-
-
+const projectImages = document.querySelectorAll('.portfolio-box img');
 projectImages.forEach(image => {
     image.addEventListener('mouseover', () => {
         TweenLite.to(image, 1, {opacity:0, ease:Power2.easeInOut});
